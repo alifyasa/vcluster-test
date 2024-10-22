a21c8a7cd1f9b83a77ae9bbb6e2496f2b67ffcf5
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-10-22 08:52:39 +0700

    fix: Debug logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,24 +2,24 @@ import { ISettingsParam, Logger } from "tslog";
 
 let logger = new Logger({
   hideLogPositionForProduction: true,
-  minLevel: 3
+  minLevel: 3,
+  prefix: ["OLD"],
 });
 
 function setLogger(settings: ISettingsParam<unknown>) {
   logger = new Logger({
     hideLogPositionForProduction: true,
+    prefix: ["OVERWRITTEN"],
     ...settings,
   });
-  logger.debug(`Set Logger with settings ${JSON.stringify(settings)}`)
+  logger.debug(`Set Logger with settings ${JSON.stringify(settings)}`);
 }
 
 function useLogger() {
-    return {
-        logger,
-        setLogger
-    }
+  return {
+    logger,
+    setLogger,
+  };
 }
 
-export {
-    useLogger
-}
+export { useLogger };
