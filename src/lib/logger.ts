9422a68c5c6806a87a0e08ae94c4f8d620d779ad
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-10-22 09:44:29 +0700

    fix: Remove debug statement

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -3,13 +3,11 @@ import { ISettingsParam, Logger } from "tslog";
 let logger = new Logger({
   hideLogPositionForProduction: true,
   minLevel: 3,
-  prefix: ["OLD"],
 });
 
 function setLogger(settings: ISettingsParam<unknown>) {
   logger = new Logger({
     hideLogPositionForProduction: true,
-    prefix: ["OVERWRITTEN"],
     ...settings,
   });
   logger.debug(`Set Logger with settings ${JSON.stringify(settings)}`);
