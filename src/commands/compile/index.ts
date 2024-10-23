7c726b72d45b0cbf48e95161ec0c184168563178
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-10-23 10:52:48 +0700

    fix: Nanoid helper

--- src/commands/compile/index.ts
+++ src/commands/compile/index.ts
@@ -1,15 +1,21 @@
 import Handlebars from "handlebars";
-import { customAlphabet, urlAlphabet } from "nanoid";
+import { customAlphabet } from "nanoid";
 import { v4 as uuidv4 } from "uuid";
 
 function createHandlebarsInstance(): typeof Handlebars {
   const handlebarsInstance = Handlebars.create();
 
   handlebarsInstance.registerHelper("uuidv4", () => uuidv4());
-  handlebarsInstance.registerHelper("nanoid", customAlphabet(urlAlphabet, 8));
+  handlebarsInstance.registerHelper("shortId", () => {
+    return customAlphabet("0123456789qwertyuiopasdfghjklzxcvbnm", 4)()
+  });
   handlebarsInstance.registerHelper("utcTimestamp", () =>
     new Date().toISOString()
   );
+  handlebarsInstance.registerHelper("unixTimestamp", () =>
+    Math.floor(Date.now() / 1000)
+  );
+
 
   return handlebarsInstance;
 }
